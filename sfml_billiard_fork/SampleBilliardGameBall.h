4da29d39ac296ebafcbacf85fb4377e1aba21a3b
ChoiOnion <117707375+ChoiOnion@users.noreply.github.com>
2022-11-09 23:04:25 +0900

    Update SampleBilliardGameBall.h

--- sfml_billiard_fork/SampleBilliardGameBall.h
+++ sfml_billiard_fork/SampleBilliardGameBall.h
@@ -1,68 +1,32 @@
 #pragma once
-#include <iostream>
-#include <vector> 
+#include <cmath>
+#include <string>
 #include <SFML/Graphics.hpp>
 #include <SFML/Graphics/CircleShape.hpp>
 #include <SFML/Graphics/Color.hpp>
 #include <SFML/Graphics/Texture.hpp>
 #include <SFML/Window.hpp>
 #include <SFML/System.hpp>
-#include "SampleBilliardObject.h"
-class SampleBilliardBoard : public SampleBilliardObject
+#include "SampleBilliardBall.h"
+class SampleBilliardGameBall : public SampleBilliardBall
 {
-public:
-	// 내부클래스 - 당구대 물리적 경계
-	class Border
-	{
-	public:
-		Border(float x1, float y1, float x2, float y2)
-		{
-			this->points.setPrimitiveType(sf::LineStrip);
-			this->points.append(sf::Vertex(sf::Vector2f(x1, y1)));
-			this->points.append(sf::Vertex(sf::Vector2f(x2, y2)));
-		}
-		~Border(void) { points.clear(); }
-		const sf::VertexArray& getPoints(void) const { return points; }
-		sf::VertexArray points;
-	};
-	//구멍
-	class Hole 
-	{
-	public:
-		Hole(float radius, float a1, float a2, float b1, float b2);
-		{
-			this->points.setPrimitiveType(sf::LineStrip);
-			this->points.append(sf::Vertex(sf::Vector2f(a1, b1)));
-			this->points.append(sf::Vertex(sf::Vector2f(a2, yb)));
-			this->points.append(sf::Vertex(sf::Vector2f(a2, b2)));
-		}
-		~Hole(void) { points.clear(); }
-		const sf::VertexArray& getPoints(void) const { return points; }
-		sf::VertexArray points;
-	};
 public: 
-	// 생성자 
-	SampleBilliardBoard(void);
-	// 소멸자 
-	~SampleBilliardBoard(void);
-	//hole에 공 들어가면 1 반환 아니면 0
-	virtual bool isHole();
-	virtual void getHole();
-	virtual void setHole();
-	
-	// Sample Game의 객체들은 반드시 상태 갱신 함수 구현해야 함 
-	virtual void update(float timeElapsed);
-	// Sample Game의 객체들은 반드시 충돌 물리 구현해야 함
-	virtual void collide(SampleBilliardObject& other);
-	// Sample Game의 객체들은 반드시 렌더링 함수 구현해야 함  
-	virtual void render(sf::RenderTarget& target);
-	// 당구대를 구성하는 경계 요소 반환 
-	const std::vector<SampleBilliardBoard::Border>& getBorders(void) const;
+	using SampleBilliardBall::SampleBilliardBall;
+	bool isIntersecting(sf::Vector2f point);
+	bool isPlayable(void);
+	void setPlayable(bool playable);
+	void setOwner(std::string owner);
+	bool isOwner(std::string owner);
+	std::string getOwner(void);
+
+	// 게임 공에 숫자를 그리기 위해 virtual 함수 재정의 
+	// 게임 공에 숫자를 그리기 위해 virtual 함수 재정의 
+	void render(sf::RenderTarget& window);
+
+	void setNumber(std::string number);
+
 private:
-	// 당구대를 나타내는 스프라이트 이미지 
-	sf::Sprite			sprite;
-	// 당구대 텍스처 데이터
-	sf::Texture			texture;
-	// 당구대 물리적 경계
-	std::vector<SampleBilliardBoard::Border>	borderLines;
+	bool playable;
+	std::string owner;
+	std::string number;
 };
