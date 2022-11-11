8ed05ba0116290136d0f9a3ca1e6118d9b098c8c
seojin <117826187+seojin0806@users.noreply.github.com>
2022-11-11 11:49:54 +0900

    잘 못 적어서 두 번 선언된 거 수정했습니다.

--- sfml_billiard_fork/SampleBilliardPlayer.cpp
+++ sfml_billiard_fork/SampleBilliardPlayer.cpp
@@ -1,41 +1,6 @@
 #include "SampleBilliardPlayer.h"
 #include "SampleGame.h"
 #include "SampleBilliardBall.h"
-
-//생성자
-SampleBilliardPlayer::SampleBilliardPlayer() {
-
-}; //기본 생성자
-SampleBilliardPlayer::SampleBilliardPlayer(sf::Vector2f center) {
-
-}; //복사 생성자, 받을 것들 생각해두기.
-
-
-//getter,setter
-void SampleBilliardPlayer::setOwner(std::string owner) {
-	this->owner = owner;
-};
-bool SampleBilliardPlayer::isOwner(std::string owner) {
-	return owner.compare(this->owner) == 0;
-};
-std::string SampleBilliardPlayer::getOwner(void) {
-	return owner;
-};
-
-
-//1p,2p를 표기하기 위한 렌더함수
-void SampleBilliardPlayer::render(sf::RenderTarget& target) {
-	sf::Text PlayerText;
-	PlayerText.setFont(SampleGame::getFont());
-	PlayerText.setFillColor(sf::Color::Black);
-	PlayerText.setString(owner[0]);
-	PlayerText.setCharacterSize(18);
-	PlayerText.setPosition(getPosition() - sf::Vector2f(5, 12));
-	target.draw(PlayerText);
-};
-#include "SampleBilliardPlayer.h"
-#include "SampleGame.h"
-#include "SampleBilliardBall.h"
 #include "GameText.h"
 
 int SampleBilliardPlayer::ismyball(std::string ball) {				//스트라이프=1, 솔리드=0, 8번볼=8로 수정해뒀습니다 (빌드 오류 방지)
