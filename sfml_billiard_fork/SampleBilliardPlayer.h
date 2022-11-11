e5c3567e653fe157f882b0d4de60816951d3780f
seojin <117826187+seojin0806@users.noreply.github.com>
2022-11-11 10:36:10 +0900

    player.h 복구

--- sfml_billiard_fork/SampleBilliardPlayer.h
+++ sfml_billiard_fork/SampleBilliardPlayer.h
@@ -24,6 +24,16 @@ public:
 	// 게임 공에 숫자를 그리기 위해 virtual 함수 재정의 
 	void render(sf::RenderTarget& window);
 
+	//플레이어 공격권 표시
+	int ismyball(std::string ball); // 다른 객체를 어떻게 판별할 것인지 
+
 private:
 	std::string owner;
+
+	//각각의 플레이어 턴을 판별하기 위한 변수
+	bool turn1p = true;
+	bool turn2p = false;
+	//서로 어떤 공을 넣어야하는지 판별하는 변수(타입은 임시)
+	std::string ball1p;
+	std::string ball2p;
 };
