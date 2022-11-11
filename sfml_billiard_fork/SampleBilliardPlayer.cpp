0d25068a7e2fda15a6dc3c181676d52c388529d9
seojin <117826187+seojin0806@users.noreply.github.com>
2022-11-11 10:57:11 +0900

    Update SampleBilliardPlayer.cpp

--- sfml_billiard_fork/SampleBilliardPlayer.cpp
+++ sfml_billiard_fork/SampleBilliardPlayer.cpp
@@ -1,40 +1,6 @@
 #include "SampleBilliardPlayer.h"
 #include "SampleGame.h"
 #include "SampleBilliardBall.h"
-
-//생성자
-SampleBilliardPlayer::SampleBilliardPlayer() {
-
-}; //기본 생성자
-SampleBilliardPlayer::SampleBilliardPlayer(sf::Vector2f center) {
-
-}; //복사 생성자, 받을 것들 생각해두기.
-
-
-//getter,setter
-void SampleBilliardPlayer::setOwner(std::string owner) {
-	this->owner = owner;
-};
-bool SampleBilliardPlayer::isOwner(std::string owner) {
-	return owner.compare(this->owner) == 0;
-};
-std::string SampleBilliardPlayer::getOwner(void) {
-	return owner;
-};
-
-
-//1p,2p를 표기하기 위한 렌더함수
-void SampleBilliardPlayer::render(sf::RenderTarget& target) {
-	sf::Text PlayerText;
-	PlayerText.setFont(SampleGame::getFont());
-	PlayerText.setFillColor(sf::Color::Black);
-	PlayerText.setString(owner[0]);
-	PlayerText.setCharacterSize(18);
-	PlayerText.setPosition(getPosition() - sf::Vector2f(5, 12));
-	target.draw(PlayerText);
-};#include "SampleBilliardPlayer.h"
-#include "SampleGame.h"
-#include "SampleBilliardBall.h"
 #include "GameText.h"
 
 //getter,setter
